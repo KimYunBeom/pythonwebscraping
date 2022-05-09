0206943bbe545670ec3ca597cfc1159585b1913c
KimYunBeom <yunbeom.kim@bespinglobal.com>
2022-05-09 14:54:43 +0900

    BeautifulSoup4 활용 1-2

--- webscraping_basic/8_bs4_gauss.py
+++ webscraping_basic/8_bs4_gauss.py
@@ -7,15 +7,27 @@ res.raise_for_status()
 
 soup = BeautifulSoup(res.text, 'lxml')
 
-cartoons = soup.find_all('td', attrs={'class':'title'})
-title = cartoons[0].a.get_text()
+# cartoons = soup.find_all('td', attrs={'class':'title'})
+# title = cartoons[0].a.get_text()
 # title = cartoons[1].a.get_text()
 # link = cartoons[0].a['href']
 # print(title)
 # print('https://comic.naver.com' + link)
 
+# 만화 제목 + 링크 가져오기
+# for cartoon in cartoons:
+#   title = cartoon.a.get_text()
+#   link = 'https://comic.naver.com' + cartoon.a['href']
+#   print(title)
+#   print(link)
+
+# 평점 구하기
+total_rates = 0
+cartoons: any = soup.find_all('div', attrs={'class':'rating_type'})
 for cartoon in cartoons:
-  title = cartoon.a.get_text()
-  link = 'https://comic.naver.com' + cartoon.a['href']
-  print(title)
-  print(link)
+  rate = cartoon.find('strong').get_text()
+  print(rate)
+  total_rates += float(rate)
+
+print('전체 점수 :', total_rates)
+print('평균 점수 :', total_rates / len(cartoons))
