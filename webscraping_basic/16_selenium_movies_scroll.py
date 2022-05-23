59a4c7132fc1bd1685084298ceae47ccbfcd3fbb
KimYunBeom <yunbeom.kim@bespinglobal.com>
2022-05-23 10:30:56 +0900

    Selenium 활용 2-3

--- webscraping_basic/16_selenium_movies_scroll.py
+++ webscraping_basic/16_selenium_movies_scroll.py
@@ -1,22 +1,14 @@
+import time
 from selenium import webdriver
+from bs4 import BeautifulSoup
 
 browser = webdriver.Chrome()
 browser.maximize_window()
 
 # 페이지 이동
 url = 'https://play.google.com/store/movies'
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Safari/537.36', 'Accept-Language': 'ko-KR,ko'}
 browser.get(url)
 
-# 지정한 위치로 스크롤 내리기
-# 모니터(해상도)의 높이인 1080 위치로 스크롤 내리기
-# browser.execute_script('window.scrollTo(0, 1080)') # 1920 x 1080
-# browser.execute_script('window.scrollTo(0, 2080)')
-
-# 화면 가장 아래로 스크롤 내리기
-# browser.execute_script('window.scrollTo(0, document.body.scrollHeight)')
-
-import time
 interval = 2 # 2초에 한번씩 스크롤 내림
 
 # 현재 문서 높이를 가져와서 저장
@@ -41,3 +33,30 @@ while True:
   prev_height = curr_height
 
 print('스크롤 완료')
+
+soup = BeautifulSoup(browser.page_source, 'lxml')
+
+movies = soup.find_all('div', attrs={'class':'ULeU3b neq64b'})
+print(len(movies))
+
+for movie in movies:
+  try:
+    title = movie.find('div', attrs={'class':'Epkrse'}).get_text()
+    
+    # 할인 전 가격
+    original_price = movie.find('span', attrs={'class':'SUZt4c P8AFK'})
+    if original_price:
+      original_price = original_price.get_text()
+    else:
+      print(title, ' <할인되지 않은 영화 제외>')
+      continue
+
+    # 할인된 가격
+    price = movie.find('span', attrs={'class':'VfPpfd VixbEe'})
+
+    # 링크
+    link  = movie.find('a', attrs={'class':''})['href']
+    # 올바른 링크 : https://play.google.com + link
+  except:
+    print('예외발생')
+
