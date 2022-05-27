ec73ff674f9c3156dc418e8b349deab8af45bed5
KimYunBeom <yunbeom.kim@bespinglobal.com>
2022-05-27 15:36:30 +0900

    IT 일반 뉴스 (네이버 뉴스)

--- webscraping_project/project.py
+++ webscraping_project/project.py
@@ -1,5 +1,5 @@
 '''
-[헤드라인 뉴스]
+[IT 뉴스]
 1. 무슨 무슨 일이...
  (링크 : http://...)
 2. 어떤 어떤 일이...
@@ -47,6 +47,10 @@ def scrape_weather():
   print('초미세먼지 {}'.format(pm25))
   print()
 
+def print_news(index, title, link):
+    print('{}. {}'.format(index + 1, title))
+    print(' (링크 : {})'.format(link))
+
 def scrape_headline_news():
   print('[언론사별 랭킹뉴스]')
 
@@ -56,12 +60,32 @@ def scrape_headline_news():
 
   news_list = soup.find_all('ul', {'class':'rankingnews_list'})[0].find_all('a', {'class':'list_title'})
 
-  for idx, news in enumerate(news_list):
-    title = str(idx + 1) + '. ' + news.get_text().strip()
+  for index, news in enumerate(news_list):
+    title = news.get_text().strip()
     link = news['href']
-    print('{}'.format(title))
-    print(' (링크 : {})'.format(link))
+    print_news(index, title, link)
+
+def scrape_it_news():
+  print('[IT 일반]')
+
+  url = 'https://news.naver.com/main/list.naver?mode=LS2D&mid=shm&sid1=105&sid2=230'
+
+  soup = create_soup(url)
+
+  news_list = soup.find('ul', {'class':'type06_headline'}).find_all('li', limit=5)
+
+  for index, news in enumerate(news_list):
+    a_idx = 0
+    img = news.find('img')
+    if img:
+      a_idx = 1 # img 태그가 있으면 1번째 img 태그의 정보를 사용
+    
+    a_tag = news.find_all('a')[a_idx]
+    title = a_tag.get_text().strip()
+    link = a_tag['href']
+    print_news(index, title, link)
 
 if __name__ == '__main__':
   # scrape_weather() # 오늘의 날씨 정보 가져오기
-  scrape_headline_news()
+  # scrape_headline_news()
+  scrape_it_news()
